6cb98776fe7031bee876f0cda4cf627abe7b04e8
jefutte <jesper@cloudpuzzles.net>
2015-05-08 18:44:49 +0200

    HA DC

---
+++ DSC/DC/HADC/HADC.ps1
@@ -0,0 +1,79 @@
+# A configuration to Create High Availability Domain Controller 
+
+$passwd = Read-Host -AsSecureString
+$domainCred = New-Object System.Management.Automation.PSCredential ("scloudpuzzles\Administrator", $passwd)
+$localUser = New-Object System.Management.Automation.PSCredential ("Administrator", $passwd)
+
+configuration HADC
+{
+    Import-DscResource -ModuleName xActiveDirectory
+
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename
+    {
+        WindowsFeature ADInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+        xADDomain FirstDC
+        {
+            DomainName = $Node.DomainName
+            DomainAdministratorCredential = $domaincred
+            SafemodeAdministratorPassword = $domaincred
+            DependsOn = "[WindowsFeature]ADInstall"
+        }
+
+        xWaitForADDomain WaitDomain
+        {
+            DomainName = $Node.DomainName
+            DomainUserCredential = $domaincred
+            RetryCount = $Node.RetryCount
+            RetryIntervalSec = $Node.RetryIntervalSec
+            DependsOn = "[xADDomain]FirstDC"
+        }
+
+        xADUser CreateUser
+        {
+            DomainName = $Node.DomainName
+            DomainAdministratorCredential = $domaincred
+            UserName = "jesper"
+            Password = $domainCred
+            Ensure = "Present"
+            DependsOn = "[xWaitForADDomain]WaitDomain"
+        }
+
+    }
+
+    Node $AllNodes.Where{$_.Role -eq "Replica DC"}.Nodename
+    {
+        WindowsFeature ADInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+        xWaitForADDomain WaitDomain
+        {
+            DomainName = $Node.DomainName
+            DomainUserCredential = $domaincred
+	   		RetryCount = $Node.RetryCount
+            RetryIntervalSec = $Node.RetryIntervalSec
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        xADDomainController SecondDC
+        {
+            DomainName = $Node.DomainName
+            DomainAdministratorCredential = $domaincred
+            SafemodeAdministratorPassword = $domaincred
+            DependsOn = "[xWaitForADDomain]WaitDomain"
+        }
+    }
+}
+
+$config = Invoke-Expression (Get-content $PSScriptRoot\HADCconfiguration.psd1 -Raw)
+HADC -configurationData $config
+
+Start-DscConfiguration -Wait -Force -Verbose -ComputerName "DC01" -Path $PSScriptRoot\AssertHADC -Credential $localcred
+Start-DscConfiguration -Wait -Force -Verbose -ComputerName "DC01" -Path $PSScriptRoot\AssertHADC -Credential $localcred

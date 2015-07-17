745aaba0b2db1e1fd27a9cc45172995911a5fb2e
Jesper Jensen <jj@solvoit.com>
2015-07-17 23:29:22 +0200

    New config

--- Azure/DNS/O365.ps1
+++ Azure/DNS/O365.ps1
@@ -1,7 +1,8 @@
-Register-AzureProvider -ProviderNamespace Microsoft.Network
-$zone = Get-AzureDnsZone -Name cloudpuzzles.net -ResourceGroupName JJ
-$rs = Get-AzureDnsRecordSet -Name "@" -RecordType TXT -Zone $zone
-Add-AzureDnsRecordConfig -RecordSet $rs -Value "MS=ms12345678"
+New-AzureDnsZone -Name cloudpuzzles.net -ResourceGroupName cloudpuzzles
+$zone = Get-AzureDnsZone -Name cloudpuzzles.net -ResourceGroupName cloudpuzzles
+Get-AzureDnsRecordSet –Name “@” –RecordType NS –Zone $zone
+$rs = Get-AzureDnsRecordSet -Name awverify -RecordType CNAME -Zone $zone
+Add-AzureDnsRecordConfig -RecordSet $rs -Cname awverify.cloudpuzzles.azurewebsites.net
 Set-AzureDnsRecordSet -RecordSet $rs
 $rs = New-AzureDnsRecordSet -Name "sip" -RecordType CNAME -Zone $zone -Ttl 3600
 Add-AzureDnsRecordConfig -RecordSet $rs -Cname "sipdir.online.lync.com"
@@ -23,4 +24,7 @@ Add-AzureDnsRecordConfig -RecordSet $rs –Priority 100 –Weight 1 –Port 443 
 Set-AzureDnsRecordSet -RecordSet $rs
 $rs = New-AzureDnsRecordSet -Name "_sipfederationtls._tcp" -RecordType SRV -Zone $zone -Ttl 3600
 Add-AzureDnsRecordConfig -RecordSet $rs –Priority 100 –Weight 1 –Port 5061 –Target "sipfed.online.lync.com"
+Set-AzureDnsRecordSet -RecordSet $rs
+$rs = Get-AzureDnsRecordSet -Name "@" -RecordType MX -Zone $zone
+Add-AzureDnsRecordConfig -RecordSet $rs -Exchange cloudpuzzles-net.mail.eo.outlook.com -Preference 5
 Set-AzureDnsRecordSet -RecordSet $rs

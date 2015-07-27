5b5f70c6390e96c135c6a222c2c5e802d31ad895
Jesper Jensen <jj@solvoit.com>
2015-07-27 21:53:47 +0200

    ChildZone

---
+++ Azure/DNS/ChildZone.ps1
@@ -0,0 +1,8 @@
+﻿Switch-AzureMode AzureServiceManagement
+$child = New-AzureDnsZone -Name aadtest.cloudpuzzles.net -ResourceGroupName cloudpuzzles
+$childnsrs = Get-AzureDnsRecordSet -Zone $child -Name "@" -RecordType NS
+
+$parent = Get-AzureDnsZone -Name cloudpuzzles.net -ResourceGroupName cloudpuzzles
+$parentnsrs = New-AzureDnsRecordSet -Zone $parent -Name "aadtest" -RecordType NS -Ttl 3600
+$parentnsrs.Records = $childnsrs.Records
+Set-AzureDnsRecordSet -RecordSet $parentnsrs

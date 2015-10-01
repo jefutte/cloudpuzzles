5299fb46762a500f205ecb0f0eac591ebe48f280
Jesper Jensen <jj@solvoit.com>
2015-10-01 22:55:34 +0200

    Tags

---
+++ Azure/Tags/Tags.ps1
@@ -0,0 +1,9 @@
+﻿#Do tags with PowerShell
+$rg = "cloudpuzzles"
+$location = "West Europe"
+
+New-AzureNetworkSecurityGroup -Name testnsg -ResourceGroupName $rg -Location $location -Tag @( @{Name="Subscription";Value="cloudpuzzles"} )
+
+$tags = (Get-AzureResourceGroup -Name $rg).Tags
+$tags += @{Name="Test";Value="yes"}
+Set-AzureResourceGroup $rg -Tag $tags
